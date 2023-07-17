4e5f85f01e829368c8b72510d3768264b534711a
Hydra <ryanandrews135@gmail.com>
2023-07-17 02:30:13 -0600

    Add local reference to Texture and Fontstring objects

--- LibMotion.lua
+++ LibMotion.lua
@@ -1193,6 +1193,9 @@ local InterpolateRGB = function(p, r1, g1, b1, r2, g2, b2)
 	return r1 + (r2 - r1) * p, g1 + (g2 - g1) * p, b1 + (b2 - b1) * p
 end
 
+local Texture = Updater:CreateTexture()
+local FontString = Updater:CreateFontString()
+
 local ColorSet = {
 	backdrop = Updater.SetBackdropColor,
 	border = Updater.SetBackdropBorderColor,
